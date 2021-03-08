616f31a2c2e7b89337c25abceb307dfb0a1ee9b1
zurmad <pablo.diazv@pucp.edu.pe>
2021-03-08 14:03:36 -0500

    Create input and output just at the beginning

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from aimodels.speechenhancement.speechenhancement_production import \
+    prediction_production
 import os
 import sys
 import threading
@@ -8,10 +10,10 @@ import numpy as np
 import pyaudio
 from pydub import AudioSegment
 from pydub.playback import play
-
-
-
 from rich.console import Console
+import alsaaudio  # only for Linux
+
+
 console = Console()
 
 # Import models: here we add new AI models.
@@ -22,8 +24,6 @@ console = Console()
 """
 1.2 Production mode
 """
-from aimodels.speechenhancement.speechenhancement_production import prediction_production
-
 
 """
 2. GLOBAL CONFIGS.
@@ -40,9 +40,9 @@ FORMAT_OUTPUT = pyaudio.paFloat32  # default LE_32bits or pyaudio.paFloat32
 channels = 1  # default mono
 sample_rate_input = 44100  # default 44100
 sample_rate_output = 8000  # default 8000 due restrictions, can't be more by 2021-03-02
-record_seconds = 2.5  # default 1.1 due some restrictions
+record_seconds = 1.2  # default 1.1 due some restrictions
 x = 0
-y=0
+y = 0
 console.print("[GLOBAL CONFIGS] Completed.", style="bold green")
 
 """
@@ -50,31 +50,27 @@ console.print("[GLOBAL CONFIGS] Completed.", style="bold green")
 """
 
 
-def record_one_second(filename_path='temporal/input.wav',thread_name=''):
+def record_one_second(filename_path='temporal/input.wav', thread_name='',p='',stream=''):
     """
     This script record around one second with pyaudio with specific finename_path
     Example taken from https://www.thepythoncode.com/article/play-and-record-audio-sound-in-python
     """
-    console.print("{}[RECORD] Recording on {}".format(thread_name,filename_path), style="bold red")
-    p = pyaudio.PyAudio()  # initialize PyAudio object
-    stream = p.open(format=FORMAT_INPUT,
-                    channels=channels,
-                    rate=sample_rate_input,
-                    input=True,
-                    output=True,
-                    frames_per_buffer=chunk)  # open stream object as input & output
+    console.print("{}[RECORD] Recording on {}".format(
+        thread_name, filename_path), style="bold red")
+
+    p=p
+    stream=stream
     frames = []
-    
+
     for i in range(int(44100 / chunk * record_seconds)):
         data = stream.read(chunk)
         # if you want to hear your voice while recording
         # stream.write(data)
         frames.append(data)
-    
 
-    stream.stop_stream()  # stop and close stream
-    stream.close()
-    p.terminate()  # terminate pyaudio object
+    # stream.stop_stream()  # stop and close stream
+    # stream.close()
+    # p.terminate()  # terminate pyaudio object
 
     # save audio file in 'write bytes' mode
     wf = wave.open(filename_path, "wb")
@@ -83,10 +79,11 @@ def record_one_second(filename_path='temporal/input.wav',thread_name=''):
     wf.setframerate(sample_rate)  # set the sample rate
     wf.writeframes(b"".join(frames))  # write the frames as bytes
     wf.close()  # close the file
-    console.print("{}[RECORD] Finished recording on {}".format(thread_name,filename_path), style="bold red")
+    console.print("{}[RECORD] Finished recording on {}".format(
+        thread_name, filename_path), style="bold red")
 
 
-def executeprediction(aimodel='speechenhancement', number=-1,thread_name=''):
+def executeprediction(aimodel='speechenhancement', number=-1, thread_name=''):
     """
     This function is a manager of ai models applied
     like filters to noisy mic
@@ -95,7 +92,7 @@ def executeprediction(aimodel='speechenhancement', number=-1,thread_name=''):
     # Speech-Enhancement model
     if aimodel == 'speechenhancement':
         console.print(
-            "{}[AI MODEL] {} -[INFERENCE]-cleaning on {}".format(thread_name,aimodel,'temporal/input_{}.wav'.format(number)), style="bold red")
+            "{}[AI MODEL] {} -[INFERENCE]-cleaning on {}".format(thread_name, aimodel, 'temporal/input_{}.wav'.format(number)), style="bold red")
         prediction_production(weights_path='aimodels/speechenhancement/weights',
                               name_model='model_unet',
                               audio_dir_prediction='temporal',
@@ -105,61 +102,114 @@ def executeprediction(aimodel='speechenhancement', number=-1,thread_name=''):
                               audio_output_prediction='output_{}.wav'.format(
                                   number),
                               sample_rate=8000,  # default 8000
-                              min_duration=1.1,  # default 1.1
+                              min_duration=1.0,  # default 1.1
                               frame_length=8064,  # default 8064
                               hop_length_frame=8064,  # default 8064
                               n_fft=255,  # default 255
                               hop_length_fft=63)  # default 63
         # os.remove('temporal/input_{}.wav')
-        console.print("{}[AI MODEL] Speech-Enhancement finished. Saving {}".format(thread_name,'temporal/output_{}.wav'.format(number)), style="bold red")
+        console.print("{}[AI MODEL] Speech-Enhancement finished. Saving {}".format(
+            thread_name, 'temporal/output_{}.wav'.format(number)), style="bold red")
     # Here you can add a new models
     else:
         console.print("{}[AI MODEL] No aimodel selected".format(thread_name,))
         return False
 
-def play_one_second(filename_path='temporal/output.wav',thread_name=''):
+
+def play_one_file(filename_path='temporal/output.wav', thread_name='', device=''):
     """
     This script play file
     Example adapted from https://people.csail.mit.edu/hubert/pyaudio/
     """
-    console.print("{}[PLAY] Playing {}".format(thread_name,filename_path), style="bold red")
+    console.print("{}[PLAY] Playing {}".format(
+        thread_name, filename_path), style="bold red")
     if (sys.platform == 'windows' or
             sys.platform == 'macos'):
         sound = AudioSegment.from_file(filename_path)
         play(sound)
     elif (sys.platform == 'linux'):
-        command_inference = 'python3 tools_example/pyalsaaudio_playback.py {}'.format(filename_path)
-        result = os.popen(command_inference).read()
-    console.print("{}[PLAY] Finish audio playing {}".format(thread_name,filename_path), style="bold red")
+        with wave.open(filename_path, 'rb') as f:
+            periodsize = 1000
+            data = f.readframes(periodsize)
+            while data:
+                # Read data from stdin
+                device.write(data)
+                data = f.readframes(periodsize)
 
-def record_process_play_thread(lock_record, lock_playing):
+    console.print("{}[PLAY] Finish audio playing {}".format(
+        thread_name, filename_path), style="bold red")
+
+
+def record_process_play_thread(lock_record, lock_playing, device,p,stream):
+    """
+    This function record, process and play per thread
+    so, just create more threads if audio is not enough
+    fast.
+    """
 
     global x
     global y
 
-    x=0
-    y=0
+    x = 0
+    y = 0
 
-    thread_name=threading.current_thread().name
+    thread_name = threading.current_thread().name
+    # command1='pactl load-module module-pipe-source source_name=virtmic file=/tmp/virtmic format=s32le rate=8000 channels=1'
+    # result = os.popen(command1).read()
 
     while True:
 
         lock_record.acquire()
-        record_one_second(filename_path='temporal/input_{}.wav'.format(x),thread_name=thread_name)
-        x+=1 
-        lock_record.release( )
+        record_one_second(
+            filename_path='temporal/input_{}.wav'.format(x), thread_name=thread_name,p=p,stream=stream)
+        x += 1
+        lock_record.release()
 
-        lock_playing.acquire()     
-        executeprediction(aimodel='speechenhancement', number=x-1,thread_name=thread_name)   
-        play_one_second(filename_path='temporal/output_{}.wav'.format(y),thread_name=thread_name)
-        y+=1
+
+
+
+        lock_playing.acquire()
+        executeprediction(aimodel='speechenhancement',
+                          number=x-1, thread_name=thread_name)
+        play_one_file(filename_path='temporal/output_{}.wav'.format(y),
+                        thread_name=thread_name, device=device)
+        y += 1
         lock_playing.release()
+
+def load_input_device():
+    """
+    This function initialize mic input
+    object and returns the objects get data
+    """
+
+    p = pyaudio.PyAudio()  # initialize PyAudio object
+    stream = p.open(format=FORMAT_INPUT,
+                    channels=channels,
+                    rate=sample_rate_input,
+                    input=True,
+                    output=True,
+                    frames_per_buffer=chunk)  # open stream object as input & output
+
+    return p,stream
+
+def load_output_device():
+    """
+    This function initialize the speakers output
+    object and then return it to write data into it.
+    """
+
+    device = alsaaudio.PCM(channels=1, rate=sample_rate_output,
+                           format=32, periodsize=1000, device='default')
+
+    return device
+
+
 
 
 def hello():
     console.print("""------------------------------------------------------------------------------------
- ______   ______   ______   __        ________   ______   __    __  ________  _______  
-/      \ |      \ /      \ |  \      |        \ /      \ |  \  |  \|        \|       \ 
+  ______   ______   ______   __        ________   ______   __    __  ________  _______  
+ /      \ |      \ /      \ |  \      |        \ /      \ |  \  |  \|        \|       \ 
 |  $$$$$$\ \$$$$$$|  $$$$$$\| $$      | $$$$$$$$|  $$$$$$\| $$\ | $$| $$$$$$$$| $$$$$$$\ 
 | $$__| $$  | $$  | $$   \$$| $$      | $$__    | $$__| $$| $$$\| $$| $$__    | $$__| $$
 | $$    $$  | $$  | $$      | $$      | $$  \   | $$    $$| $$$$\ $$| $$  \   | $$    $$
@@ -168,7 +218,12 @@ def hello():
 | $$  | $$|   $$ \ \$$    $$| $$     \| $$     \| $$  | $$| $$  \$$$| $$     \| $$  | $$
 \ $$   \$$ \$$$$$$  \$$$$$$  \$$$$$$$$ \$$$$$$$$ \$$   \$$ \$$   \$$ \$$$$$$$$ \$$   \$$
                                                                                         
-                                                                                        """, style="bold red")
+Mantainer: Pablo Diaz
+Github: https://github.com/DZDL/aicleaner
+License: MIT
+Version: v0.1 [2020-03-08]
+Branch: Multithreading one way
+------------------------------------------------------------------------------------""", style="bold yellow")
 
 
 """
@@ -181,23 +236,25 @@ if __name__ == '__main__':
     lock_playing = threading.Lock()
 
     hello()
+    
+    device = load_output_device()  # only for linux pyalsaaudio
+    p,stream = load_input_device() # only for linux pyalsaaudio
 
     # start_time = time.time()
     # Record voice and generate temporal/input_x.wav, process and play
     t1 = threading.Thread(target=record_process_play_thread, args=(
-        lock_record, lock_playing), name='[T1]')
+        lock_record, lock_playing, device,p,stream), name='[T1]')
     t2 = threading.Thread(target=record_process_play_thread, args=(
-        lock_record, lock_playing), name='[T2]')
+        lock_record, lock_playing, device,p,stream), name='[T2]')
     t3 = threading.Thread(target=record_process_play_thread, args=(
-        lock_record, lock_playing), name='[T3]')
+        lock_record, lock_playing, device,p,stream), name='[T3]')
     t4 = threading.Thread(target=record_process_play_thread, args=(
-        lock_record, lock_playing), name='[T4]')
+        lock_record, lock_playing, device,p,stream), name='[T4]')
 
     t1.start()
     t2.start()
     t3.start()
     t4.start()
-
 
     t1.join()
     t2.join()
---
+++ tools_example/pyalsaaudio_playback_continous.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+# -*- mode: python; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*-
+# Taken from https://github.com/larsimmisch/pyalsaaudio/blob/master/playwav.py
+# Simple test script that plays (some) wav files
+
+from __future__ import print_function
+
+import getopt
+import sys
+import wave
+
+import alsaaudio
+
+
+def play(device, f):
+
+    format = None
+
+    # 8bit is unsigned in wav files
+    if f.getsampwidth() == 1:
+        format = alsaaudio.PCM_FORMAT_U8
+    # Otherwise we assume signed data, little endian
+    elif f.getsampwidth() == 2:
+        format = alsaaudio.PCM_FORMAT_S16_LE
+    elif f.getsampwidth() == 3:
+        format = alsaaudio.PCM_FORMAT_S24_3LE
+    elif f.getsampwidth() == 4:
+        format = alsaaudio.PCM_FORMAT_S32_LE
+    else:
+        raise ValueError('Unsupported format')
+
+    periodsize = f.getframerate() // 8
+
+    print('%d channels, %d sampling rate, format %d, periodsize %d\n' % (f.getnchannels(),
+                                                                         f.getframerate(),
+                                                                         format,
+                                                                         periodsize))
+
+    device = alsaaudio.PCM(channels=f.getnchannels(), rate=f.getframerate(
+    ), format=format, periodsize=periodsize, device=device)
+
+    data = f.readframes(periodsize)
+    while data:
+        # Read data from stdin
+        device.write(data)
+        data = f.readframes(periodsize)
+
+
+def usage():
+    print('usage: playwav.py [-d <device>] <file>', file=sys.stderr)
+    sys.exit(2)
+
+
+if __name__ == '__main__':
+
+    device = 'default'  # HERE YOU CHANGE THE AUDIO OUTPUT
+
+    opts, args = getopt.getopt(sys.argv[1:], 'd:')
+    for o, a in opts:
+        if o == '-d':
+            device = a
+
+    if not args:
+        usage()
+
+    with wave.open(args[0], 'rb') as f:
+        play(device, f)
